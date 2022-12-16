cd3de5fb608ab04e5e5bc39fd209db6ed71d42ef
Simo Tukiainen <simo.tukiainen@fmi.fi>
2022-12-16 11:13:32 +0200

    Adjust imports

--- voodoonet/__init__.py
+++ voodoonet/__init__.py
@@ -1,3 +1,3 @@
-from voodoonet import utils
-
 from .loader import generate_training_data, load_training_data, run, save_training_data
+from .torch_model import VoodooNet
+from .utils import VoodooOptions, VoodooTrainingOptions

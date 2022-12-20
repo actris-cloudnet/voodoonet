0b975d3c393fb4b353a54025ec7df078376efe1d
Simo Tukiainen <simo.tukiainen@fmi.fi>
2022-12-20 11:11:56 +0200

    Add import

--- voodoonet/__init__.py
+++ voodoonet/__init__.py
@@ -1,3 +1,8 @@
-from .loader import generate_training_data, infer, train
+from .loader import (
+    generate_training_data,
+    generate_training_data_for_cloudnet,
+    infer,
+    train,
+)
 from .torch_model import VoodooNet
 from .utils import VoodooOptions, VoodooTrainingOptions, WandbConfig
